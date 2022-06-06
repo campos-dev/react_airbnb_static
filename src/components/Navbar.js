c3067786f3d4e1e21ce350dd33f0345627e40926
Marcelo Campos <camposwebdev@gmail.com>
2022-06-06 20:31:23 -0300

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img
+        src={require("../images/airbnb-logo.png")}
+        className="nav--logo"
+        alt="logo airbnb"
+      />
+    </nav>
+  );
+}
